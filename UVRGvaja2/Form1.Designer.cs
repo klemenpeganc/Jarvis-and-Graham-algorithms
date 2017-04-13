8ba923916b3e4d88a449019b5f9e0a521fe04a4b
klemenpeganc <peganc.klemen@gmail.com>
2017-04-13 22:19:34 +0200

    Jarvis March, Graham Scan

---
+++ UVRGvaja2/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace UVRGvaja2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tbNumPts = new System.Windows.Forms.TextBox();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.rbEqual = new System.Windows.Forms.RadioButton();
+            this.rbNormal = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbJarvis = new System.Windows.Forms.RadioButton();
+            this.rbGraham = new System.Windows.Forms.RadioButton();
+            this.rbQuickhull = new System.Windows.Forms.RadioButton();
+            this.btnHull = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbTime = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 800);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tbNumPts
+            // 
+            this.tbNumPts.Location = new System.Drawing.Point(962, 98);
+            this.tbNumPts.Name = "tbNumPts";
+            this.tbNumPts.Size = new System.Drawing.Size(67, 22);
+            this.tbNumPts.TabIndex = 1;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(921, 126);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(108, 34);
+            this.btnDraw.TabIndex = 2;
+            this.btnDraw.Text = "Narisi tocke";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // rbEqual
+            // 
+            this.rbEqual.Location = new System.Drawing.Point(1, 12);
+            this.rbEqual.Name = "rbEqual";
+            this.rbEqual.Size = new System.Drawing.Size(153, 41);
+            this.rbEqual.TabIndex = 3;
+            this.rbEqual.TabStop = true;
+            this.rbEqual.Text = "Enakomerna porazdelitev";
+            this.rbEqual.UseVisualStyleBackColor = true;
+            // 
+            // rbNormal
+            // 
+            this.rbNormal.Location = new System.Drawing.Point(6, 55);
+            this.rbNormal.Name = "rbNormal";
+            this.rbNormal.Size = new System.Drawing.Size(122, 43);
+            this.rbNormal.TabIndex = 4;
+            this.rbNormal.TabStop = true;
+            this.rbNormal.Text = "Normalna porazdelitev";
+            this.rbNormal.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(872, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Stevilo tock:";
+            // 
+            // rbJarvis
+            // 
+            this.rbJarvis.AutoSize = true;
+            this.rbJarvis.Location = new System.Drawing.Point(904, 170);
+            this.rbJarvis.Name = "rbJarvis";
+            this.rbJarvis.Size = new System.Drawing.Size(125, 21);
+            this.rbJarvis.TabIndex = 6;
+            this.rbJarvis.TabStop = true;
+            this.rbJarvis.Text = "Jarvisov obhod";
+            this.rbJarvis.UseVisualStyleBackColor = true;
+            // 
+            // rbGraham
+            // 
+            this.rbGraham.Location = new System.Drawing.Point(904, 197);
+            this.rbGraham.Name = "rbGraham";
+            this.rbGraham.Size = new System.Drawing.Size(125, 46);
+            this.rbGraham.TabIndex = 7;
+            this.rbGraham.TabStop = true;
+            this.rbGraham.Text = "Grahamovo prebiranje";
+            this.rbGraham.UseVisualStyleBackColor = true;
+            // 
+            // rbQuickhull
+            // 
+            this.rbQuickhull.Location = new System.Drawing.Point(904, 238);
+            this.rbQuickhull.Name = "rbQuickhull";
+            this.rbQuickhull.Size = new System.Drawing.Size(136, 71);
+            this.rbQuickhull.TabIndex = 8;
+            this.rbQuickhull.TabStop = true;
+            this.rbQuickhull.Text = "Hitra konveksna lupina";
+            this.rbQuickhull.UseVisualStyleBackColor = true;
+            // 
+            // btnHull
+            // 
+            this.btnHull.Location = new System.Drawing.Point(921, 303);
+            this.btnHull.Name = "btnHull";
+            this.btnHull.Size = new System.Drawing.Size(108, 34);
+            this.btnHull.TabIndex = 9;
+            this.btnHull.Text = "Narisi lupino";
+            this.btnHull.UseVisualStyleBackColor = true;
+            this.btnHull.Click += new System.EventHandler(this.btnHull_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbEqual);
+            this.groupBox1.Controls.Add(this.rbNormal);
+            this.groupBox1.Location = new System.Drawing.Point(875, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(154, 98);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // tbTime
+            // 
+            this.tbTime.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tbTime.Location = new System.Drawing.Point(816, 343);
+            this.tbTime.Multiline = true;
+            this.tbTime.Name = "tbTime";
+            this.tbTime.ReadOnly = true;
+            this.tbTime.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbTime.Size = new System.Drawing.Size(213, 457);
+            this.tbTime.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1041, 853);
+            this.Controls.Add(this.tbTime);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnHull);
+            this.Controls.Add(this.rbQuickhull);
+            this.Controls.Add(this.rbGraham);
+            this.Controls.Add(this.rbJarvis);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.tbNumPts);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Vaja 2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox tbNumPts;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.RadioButton rbEqual;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbJarvis;
+        private System.Windows.Forms.RadioButton rbGraham;
+        private System.Windows.Forms.RadioButton rbQuickhull;
+        private System.Windows.Forms.Button btnHull;
+        private System.Windows.Forms.RadioButton rbNormal;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbTime;
+    }
+}
+
---
+++ UVRGvaja2/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UVRGvaja2
+{
+    public partial class Form1 : Form
+    {
+        const int POZ = 1;
+        const int NEG = -1;
+        const int KOL = 0;
+        List<PointF> pts;
+        List<PointF> hull = new List<PointF>();
+        Graphics g;
+        SolidBrush brush = new SolidBrush(Color.IndianRed);
+        Pen hullPen = new Pen(Color.SpringGreen);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            int numOfPts;
+            if (!string.IsNullOrEmpty(tbNumPts.Text))
+            {
+                numOfPts = int.Parse(tbNumPts.Text);
+                pts = new List<PointF>();
+                pts.Clear();
+                pictureBox1.Refresh();
+                DateTime start = DateTime.Now;
+                if (rbNormal.Checked)
+                {
+                    normalDistribution(numOfPts);
+                    DateTime end = DateTime.Now;
+                    TimeSpan time = end - start;
+                    tbTime.Text += "Cas risanja tock: " + time.Milliseconds + "ms.\r\n";
+                }
+                else if (rbEqual.Checked)
+                {
+                    equalDistribution(numOfPts);
+                    DateTime end = DateTime.Now;
+                    TimeSpan time = end - start;
+                    tbTime.Text += "Cas risanja tock: " + time.Milliseconds + "ms.\r\n";
+                }
+                else
+                {
+                    MessageBox.Show("Izberite kaksno razdelitev tock zelite!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vnesi stevilo tock!");
+            }
+
+        }
+        private void normalDistribution(int num)
+        {
+            g = Graphics.FromHwnd(pictureBox1.Handle);
+            Random rnd = new Random();
+            for (int i = 0; i < num; i++)
+            {
+                double x = rnd.NextDouble() + rnd.NextDouble() + rnd.NextDouble() + rnd.NextDouble();
+                x /= 4;
+                double y = rnd.NextDouble() + rnd.NextDouble() + rnd.NextDouble() + rnd.NextDouble();
+                y /= 4;
+                PointF point = new PointF((float)(x * 600), (float)(y * 600));
+                pts.Add(point);
+                g.FillRectangle(brush, pts[i].X, pts[i].Y, 3, 3);
+            }
+            g.Dispose();
+        }
+        private void equalDistribution(int num)
+        {
+            g = Graphics.FromHwnd(pictureBox1.Handle);
+            Random rnd = new Random();
+            for (int i = 0; i < num; i++)
+            {
+                double x = rnd.NextDouble();
+                double y = rnd.NextDouble();
+                PointF point = new PointF((float)(x * 500) + 50, (float)(y * 500) + 50);
+                pts.Add(point);
+                g.FillRectangle(brush, pts[i].X, pts[i].Y, 3, 3);
+            }
+            g.Dispose();
+        }
+
+        private void jarvisMarch()
+        {
+            DateTime start = DateTime.Now;
+            if (pts.Count < 3)
+            {
+                MessageBox.Show("Vnesiti morate vsaj 3 tocke!");
+            }
+            else
+            {
+                g = Graphics.FromHwnd(pictureBox1.Handle);
+                hull.Clear();
+                PointF maxYpoint = pts[0];
+                PointF minYpoint = pts[0];
+                PointF maxAnglePoint;
+                PointF minAnglePoint;
+                for (int i = 0; i < pts.Count; i++)
+                {
+                    if (pts[i].Y > maxYpoint.Y)
+                    {
+                        maxYpoint = pts[i];
+                    }
+                    if (pts[i].Y < minYpoint.Y)
+                    {
+                        minYpoint = pts[i];
+                    }
+                }
+                hull.Add(minYpoint);
+                PointF tempPoint = minYpoint;
+                while (tempPoint != maxYpoint)
+                {
+                    maxAnglePoint = tempPoint;
+                    for (int i = 0; i < pts.Count; i++)
+                    {
+                        if ((findAngle(tempPoint, maxAnglePoint) < findAngle(tempPoint, pts[i]) && (!hull.Contains(pts[i]) || pts[i] == maxYpoint)) && findAngle(tempPoint, pts[i]) <= 270)
+                        {
+                            maxAnglePoint = pts[i];
+                        }
+                    }
+                    g.DrawLine(hullPen, tempPoint, maxAnglePoint);
+                    tempPoint = maxAnglePoint;
+                    hull.Add(tempPoint);
+
+                }
+                tempPoint = minYpoint;
+                while (tempPoint != maxYpoint)
+                {
+                    minAnglePoint = maxYpoint;
+                    for (int i = 0; i < pts.Count; i++)
+                    {
+                        if (findAngle(tempPoint, minAnglePoint) > findAngle(tempPoint, pts[i]) && (!hull.Contains(pts[i]) || pts[i] == maxYpoint) && findAngle(tempPoint, pts[i]) >= 90)
+                        {
+                            minAnglePoint = pts[i];
+                        }
+                    }
+                    g.DrawLine(hullPen, tempPoint, minAnglePoint);
+                    tempPoint = minAnglePoint;
+                    if (tempPoint == maxYpoint)
+                    {
+                        break;
+                    }
+                    else
+                        hull.Add(tempPoint);
+                }
+            }
+            g.Dispose();
+            DateTime end = DateTime.Now;
+            TimeSpan time = end - start;
+            tbTime.Text += "Jarvisov obhod: " + time.Milliseconds + "ms.\r\n";
+        }
+        private void grahamScan()
+        {
+            if (pts.Count < 3)
+            {
+                MessageBox.Show("Vnesiti morate vsaj 3 tocke!");
+            }
+            else
+            {
+                hull.Clear();
+                DateTime start = DateTime.Now;
+                g = Graphics.FromHwnd(pictureBox1.Handle);
+                PointF temp = new PointF(pts[0].X, pts[0].Y);
+                int indeks = 0;
+                for (int i = 0; i < pts.Count; i++)
+                {
+                    if (pts[i].Y < temp.Y)
+                    {
+                        temp = pts[i];
+                        indeks = i;
+                    }
+                    else if (pts[i].Y == temp.Y)
+                    {
+                        if (pts[i].X < temp.X)
+                        {
+                            temp = pts[i];
+                            indeks = i;
+                        }
+                    }
+                }
+                double[] angles = new double[pts.Count];
+                angles[indeks] = -1;
+                for (int i = 0; i < pts.Count; i++)
+                {
+                    if (i == indeks)
+                        continue;
+                    angles[i] = grahamKot(i, indeks);
+                }
+                PointF[] ptsArray = pts.ToArray();
+                Array.Sort(angles, ptsArray);
+                pts = new List<PointF>(ptsArray);
+                List<double> angleList = new List<double>(angles);
+                for (int i = pts.Count - 1; i > 0; i--)
+                {
+                    if (angleList[i] == angleList[i - 1])
+                    {
+                        if (razdalja(pts[i], pts[0]) < razdalja(pts[i - 1], pts[0]))
+                        {
+                            pts.RemoveAt(i);
+                            angleList.RemoveAt(i);
+                        }
+                        else
+                        {
+                            pts.RemoveAt(i - 1);
+                            angleList.RemoveAt(i - 1);
+                        }
+                    }
+                }
+                pts.Insert(0, pts[pts.Count - 1]);
+                int p = 1;
+                for (int i = 2; i < pts.Count; i++)
+                {
+                    while (orientacija(pts[p - 1], pts[p], pts[i]) <= KOL)
+                    {
+                        if (p > 1)
+                            p--;
+                        else if (i == pts.Count)
+                            break;
+                        else
+                            i++;
+                    }
+                    p++;
+                    PointF tempPoint = pts[i];
+                    pts[i] = pts[p];
+                    pts[p] = tempPoint;
+                }
+                for (int i = 0; i < p; i++)
+                {
+                    hull.Add(pts[i]);
+                }
+                for (int i = 0; i < p - 1; i++)
+                {
+                    g.DrawLine(hullPen, hull[i], hull[i + 1]);
+                }
+                g.DrawLine(hullPen, hull[0], hull[p - 1]);
+
+                DateTime end = DateTime.Now;
+                TimeSpan time = end - start;
+                tbTime.Text += "Graham: " + time.Milliseconds + "ms.\r\n";
+                g.Dispose();
+            }
+        }
+
+
+        private void btnHull_Click(object sender, EventArgs e)
+        {
+            if (rbJarvis.Checked)
+            {
+                jarvisMarch();
+            }
+            if (rbGraham.Checked)
+            {
+                grahamScan();
+            }
+        }
+        double findAngle(PointF a, PointF b)
+        {
+            float vector1 = b.X - a.X;
+            float vector2 = b.Y - a.Y;
+
+            if (Math.Abs(vector1) < 0.0001 && Math.Abs(vector2) < 0.0001)
+                return 0;
+
+            double angle = Math.Atan(vector2 / vector1) * 180.0 / 3.141592;
+            if (vector1 >= 0 && vector2 >= 0)
+                angle = 90 + angle;
+            else if (vector1 >= 0 && vector2 < 0)
+                angle = 90 + angle;
+            else if (vector1 < 0 && vector2 > 0)
+                angle = 270 + angle;
+            else if (vector1 < 0 && vector2 <= 0)
+                angle = 270 + angle;
+            return angle;
+
+        }
+        int orientacija(PointF p, PointF p1, PointF p2)
+        {
+            float produkt = (p1.X - p.X) * (p2.Y - p.Y) - (p1.Y - p.Y) * (p2.X - p.X); //(p1- p)x(p2 - p)
+            if (Math.Abs(produkt) < 0.0001) return KOL;
+            return produkt > 0.0001 ? POZ : NEG;
+        }
+        double grahamKot(int x, int start)
+        {
+            PointF temp = new PointF(pts[x].X - pts[start].X, pts[x].Y - pts[start].Y);
+            return Math.Acos(temp.X / Math.Sqrt(temp.X * temp.X + temp.Y * temp.Y));
+        }
+        double razdalja(PointF p1, PointF p2)
+        {
+            double distance = 0;
+            distance = (p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y);
+            distance = Math.Sqrt(Math.Abs(distance));
+            return distance;
+        }
+    }
+}
